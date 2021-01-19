c9dd1ae8f4eaecacd952080d77d889aeb8fde048
tarabyte84 <tekotthoff@me.com>
2021-01-19 15:09:45 -0600

    Moved helloForm method to templates

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
 @RequestMapping("hello")
 public class HelloController {
 
@@ -17,37 +16,34 @@ public class HelloController {
 
     //Handles requests at /hello/goodbye
     @GetMapping("goodbye")
+    @ResponseBody
     public String goodbye() {
         return "Goodbye, Spring!";
     }
 
     //Handles request of the form /hello?name=LaunchCode
     @RequestMapping(method={RequestMethod.GET, RequestMethod.POST})
+    @ResponseBody
     public String helloWithQueryParam(@RequestParam String name) {
         return "Hello " + name + "!";
     }
 
     //Handles requests of the form /hello/LaunchCode
     @GetMapping("{name}")
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 
     //Lives at /hello/form
     @GetMapping("form")
-    public String form() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" + //submit a request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+    public String helloForm() {
+        return "form";
     }
 
     //Lives at /hello/language
     @GetMapping("language")
+    @ResponseBody
     public String createMessage() {
         return "<html>" +
                 "<body>" +
@@ -66,6 +62,7 @@ public class HelloController {
     }
 
     @PostMapping("language")
+    @ResponseBody
     public String languageReply(@RequestParam String name, @RequestParam String languages) {
         String greeting;
         switch (languages) {
